5c5bfc8e6cac38e0bec94938232b361b5b388bca
Adam Tegtmeier <ategtmei@gmail.com>
2019-12-01 18:44:02 -0500

    Well, that was much too complex for what it was.

--- sizes.cpp
+++ sizes.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
+#include <stdlib.h>
+//#include <unistd.h>
+#include <time.h>
+#include <dos.h>
+#include <string>
+using namespace std;
 
 int main()
 {
 	cout << "The size of an int is:\t\t" << sizeof(int)
-		<< "bytes." << endl;
+		<< "bytes.\n";
 	cout << "The size of an short is:\t" << sizeof(short)
-		<< "bytes." << endl;
+		<< "bytes.\n";
 	cout << "The size of an long is:\t\t" << sizeof(long)
-		<< "bytes." << endl;
+		<< "bytes.\n";
 	cout << "The size of an char is:\t\t" << sizeof(char)
-		<< "bytes." << endl;
-	cout << "The size of an bool is:\t\t" << sizeof(boot)
-		<< "bytes." << endl;
+		<< "bytes.\n";
+	cout << "The size of an bool is:\t\t" << sizeof(bool)
+		<< "bytes.\n";
 	cout << "The size of an float is:\t\t" << sizeof(float)
-		<< "bytes." << endl;
+		<< "bytes.\n";
 	cout << "The size of an double is:\t" << sizeof(double)
-		<< "bytes." << endl;
+		<< "bytes.\n";
 
 	return 0;
 }
