b7ef60e4fbe0325438031e9776dfa27db6110c19
Adam Tegtmeier <ategtmei@gmail.com>
2019-12-01 18:48:24 -0500

    Made it prettier with white space.

--- sizes.cpp
+++ sizes.cpp
@@ -3,20 +3,24 @@ using namespace std;
 
 int main()
 {
+	cout << endl;
 	cout << "The size of an int is:\t\t" << sizeof(int)
-		<< "bytes.\n";
+		<< " bytes." << endl;
 	cout << "The size of an short is:\t" << sizeof(short)
-		<< "bytes.\n";
+		<< " bytes." << endl;
 	cout << "The size of an long is:\t\t" << sizeof(long)
-		<< "bytes.\n";
+		<< " bytes." << endl;
 	cout << "The size of an char is:\t\t" << sizeof(char)
-		<< "bytes.\n";
+		<< " bytes." << endl;
 	cout << "The size of an bool is:\t\t" << sizeof(bool)
-		<< "bytes.\n";
-	cout << "The size of an float is:\t\t" << sizeof(float)
-		<< "bytes.\n";
+		<< " bytes." << endl;
+
+	cout << endl;
+
+	cout << "The size of an float is:\t" << sizeof(float)
+		<< " bytes." << endl;
 	cout << "The size of an double is:\t" << sizeof(double)
-		<< "bytes.\n";
+		<< " bytes." << endl;
 
 	return 0;
 }
