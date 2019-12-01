d9d400d9a18184ee5a21ec02e70a6881c1a92001
Adam Tegtmeier <ategtmei@gmail.com>
2019-12-01 18:38:41 -0500

    Reliving the glory days...

---
+++ sizes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	cout << "The size of an int is:\t\t" << sizeof(int)
+		<< "bytes." << endl;
+	cout << "The size of an short is:\t" << sizeof(short)
+		<< "bytes." << endl;
+	cout << "The size of an long is:\t\t" << sizeof(long)
+		<< "bytes." << endl;
+	cout << "The size of an char is:\t\t" << sizeof(char)
+		<< "bytes." << endl;
+	cout << "The size of an bool is:\t\t" << sizeof(boot)
+		<< "bytes." << endl;
+	cout << "The size of an float is:\t\t" << sizeof(float)
+		<< "bytes." << endl;
+	cout << "The size of an double is:\t" << sizeof(double)
+		<< "bytes." << endl;
+
+	return 0;
+}
